a2d8d178d2718b25753a88d065aca0d8aa66531a
fatkulnurk <fatkulnurk@gmail.com>
2018-10-02 15:05:07 +0700

    0.3.1

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,20 @@ Route::get('/dashboard/user/create', function () {
     return view('dashboard.user.add');
 });
 
+Route::get('/dashboard/user/show', function () {
+    return view('dashboard.user.show');
+});
+
+
+Route::get('/dashboard/edit/{id}', function () {
+    return view('dashboard.user.edit');
+});
+
+
+Route::get('/dashboard/user/delete/{id}', function () {
+    return view('dashboard.user.destroy');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
