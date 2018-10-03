60aad49d563f58dce5caeab437fa9fb423bbe71e
nadilawh <nadilawh0@gmail.com>
2018-10-02 20:20:44 -0700

    1.0.8

--- app/Http/Controllers/ApiMaterialCategory.php
+++ app/Http/Controllers/ApiMaterialCategory.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\MaterialCategory;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use phpDocumentor\Reflection\DocBlock\Tags\Return_;
 
 class ApiMaterialCategory extends Controller
 {
-    //
+
+    public function index()
+    {
+        return MaterialCategory::all();
+    }
+
+    public function show($id){
+        $result = MaterialCategory::find($id);
+
+        if(!$result){
+            return response()->json([
+                'status'    => 'true',
+                'message' => 'error , no data'
+            ],404);
+        }else{
+            return $result;
+        }
+    }
+
+    public function store(Request $request){
+        $this->validate($request,[
+            'name'             => 'required',
+            'description'      => 'required',
+        ]);
+
+
+        $data                   = new User;
+        $data->name             = $request->name;
+        $data->description      = $request->description;
+        $data->save();
+    }
+
+    public function update(Request $request,$id){
+        $this->validate($request,[
+            'name'             => 'required',
+            'description'      => 'required',
+        ]);
+
+        $data = User::find($id);
+        $data->name             = $request->name;
+        $data->description      = $request->description;
+        $data->save();
+    }
+
+    public function destroy($id){
+        $result = MaterialCategory::find($id)->delete();
+        if($result){
+            return response()->json([
+                'status'    => 'true',
+                'message'   => 'Delete Successfuly'
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 'error , no data'
+            ],404);
+        }
+    }
 }
--- app/Http/Controllers/ApiRoles.php
+++ app/Http/Controllers/ApiRoles.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Roles;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use phpDocumentor\Reflection\DocBlock\Tags\Return_;
 
 class ApiRoles extends Controller
 {
-    //
+
+    public function index()
+    {
+        return Roles::all();
+    }
+
+    public function show($id){
+        $result = Roles::find($id);
+
+        if(!$result){
+            return response()->json([
+                'status'    => 'true',
+                'message' => 'error , no data'
+            ],404);
+        }else{
+            return $result;
+        }
+    }
+
+    public function store(Request $request){
+        $this->validate($request,[
+            'role'         => 'required|unique:roles',
+            'description'  => 'required',
+        ]);
+
+
+        $data                   = new Roles;
+        $data->role             = $request->role;
+        $data->description      = $request->description;
+        $data->save();
+    }
+
+    public function update(Request $request,$id){
+        $this->validate($request,[
+            'role'         => 'required|unique:roles',
+            'description'  => 'required',
+        ]);
+
+        $data = User::find($id);
+        $data->role             = $request->role;
+        $data->description      = $request->description;
+        $data->save();
+    }
+
+    public function destroy($id){
+        $result = Roles::find($id)->delete();
+        if($result){
+            return response()->json([
+                'status'    => 'true',
+                'message'   => 'Delete Successfuly'
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 'error , no data'
+            ],404);
+        }
+    }
 }
