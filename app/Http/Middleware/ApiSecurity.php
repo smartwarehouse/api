9e43605204246346f7240fa1270996055aadd817
fatkulnurk <fatkulnurk@gmail.com>
2018-10-04 22:32:35 +0700

    0.4.1

--- app/Http/Middleware/ApiSecurity.php
+++ app/Http/Middleware/ApiSecurity.php
@@ -15,7 +15,25 @@ class ApiSecurity
      */
     public function handle($request, Closure $next)
     {
-        //echo 'ini sebelumnya';
-        return $next($request);
+        // https://stackoverflow.com/questions/33076705/laravel-5-1-api-enable-cors
+
+        header("Access-Control-Allow-Origin: *");
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin, Authorization'
+        ];
+        if($request->getMethod() == "OPTIONS") {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return Response::make('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach($headers as $key => $value)
+            $response->header($key, $value);
+        return $response;
+
+        //return $next($request);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -27,35 +27,82 @@ use Illuminate\Http\Request;
  * Semua di autentikasi dengan Middleware
  * * * * * * * * * * * * * * * * * * * * * */
 //Route::group(['prefix' => '', 'middleware' => 'auth'], function() {
-Route::group(['middleware' => 'apisecurity'], function() {
 //Route::group(['prefix' => ''], function() {
 
-    // api user
-    Route::group(['prefix' => '/users'],function (){
 
-        Route::get('','ApiUsers@index');
+Route::group(['middleware' => 'apisecurity'], function() {
+    // api Location
+    Route::group(['prefix' => '/locations'],function (){
+        Route::get('','ApiLocation@index');
+        Route::get('/{id}','ApiLocation@show');
+        Route::post('/','ApiLocation@store');
+        Route::put('/{id}','ApiLocation@update');
+        Route::delete('/{id}','ApiLocation@destroy');
+    });
 
-        Route::get('/{id}','ApiUsers@show');
+    // api Material
+    Route::group(['prefix' => '/materials'],function (){
+        Route::get('','ApiMaterial@index');
+        Route::get('/{id}','ApiMaterial@show');
+        Route::post('/','ApiMaterial@store');
+        Route::put('/{id}','ApiMaterial@update');
+        Route::delete('/{id}','ApiMaterial@destroy');
+    });
 
-        Route::post('/','ApiUsers@store');
+    // api Material Category
+    Route::group(['prefix' => '/materialcategories'],function (){
+        Route::get('','ApiMaterialCategory@index');
+        Route::get('/{id}','ApiMaterialCategory@show');
+        Route::post('/','ApiMaterialCategory@store');
+        Route::put('/{id}','ApiMaterialCategory@update');
+        Route::delete('/{id}','ApiMaterialCategory@destroy');
+    });
 
-        Route::put('/{id}','ApiUsers@update');
+    // api Material Statistic
+    Route::group(['prefix' => '/materialstatistics'],function (){
+        Route::get('','ApiMaterialStatistic@index');
+        Route::get('/{id}','ApiMaterialStatistic@show');
+        Route::post('/','ApiMaterialStatistic@store');
+        Route::put('/{id}','ApiMaterialStatistic@update');
+        Route::delete('/{id}','ApiMaterialStatistic@destroy');
+    });
 
-        Route::delete('/{id}','ApiUsers@destroy');
+    // api rfid
+    Route::group(['prefix' => '/rfids'],function (){
+        Route::get('','ApiRfid@index');
+        Route::get('/{id}','ApiRfid@show');
+        Route::post('/','ApiRfid@store');
+        Route::put('/{id}','ApiRfid@update');
+        Route::delete('/{id}','ApiRfid@destroy');
     });
 
     // api Role
     Route::group(['prefix' => '/roles'],function (){
-
         Route::get('','ApiRoles@index');
-
         Route::get('/{id}','ApiRoles@show');
-
         Route::post('/','ApiRoles@store');
-
         Route::put('/{id}','ApiRoles@update');
-
         Route::delete('/{id}','ApiRoles@destroy');
     });
+
+    // api Type
+    Route::group(['prefix' => '/types'],function (){
+        Route::get('','ApiType@index');
+        Route::get('/{id}','ApiType@show');
+        Route::post('/','ApiType@store');
+        Route::put('/{id}','ApiType@update');
+        Route::delete('/{id}','ApiType@destroy');
+    });
+
+    // api user
+    Route::group(['prefix' => '/users'],function (){
+        Route::get('','ApiUsers@index');
+        Route::get('/{id}','ApiUsers@show');
+        Route::post('/','ApiUsers@store');
+        Route::put('/{id}','ApiUsers@update');
+        Route::delete('/{id}','ApiUsers@destroy');
+    });
+
+
 
 });
